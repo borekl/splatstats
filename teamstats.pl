c143183e512f5ebf43909868d865d00470793697
Borek Lupomesky <borek@lupomesky.cz>
2020-06-03 22:50:21 +0200

    Implement log/milestones entries filtering
    Only entries from clan members falling into specified time bracket will
    be used.

--- teamstats.pl
+++ teamstats.pl
@@ -6,6 +6,8 @@ use v5.10;
 
 use Path::Tiny;
 use JSON::MaybeXS;
+use Time::Moment;
+use Try::Tiny;
 
 
 #=== globals =================================================================
@@ -22,6 +24,15 @@ my %cfg = (
   logdir => 'logs',
 
   wget => 'wget --connect-timeout=10 --dns-timeout=5 --read-timeout=60 -t 1 -c -q -O %s %s',
+
+  match => {
+    members => [
+      'Mandevil', 'shummie', 'particleface', 'BerryKnight', 'MrMan',
+      'fearitself'
+    ],
+    start => Time::Moment->from_string('20191121T000000+0200'),
+    end   => Time::Moment->from_string('20200401T000000+0200'),
+  },
 
   servers => {
     
@@ -41,6 +52,23 @@ my %cfg = (
 );
 
 
+#=== aux functions ===========================================================
+
+# Convert time as saved in DCSS logfiles to Unix epoch time. DCSS
+# "start"/"end" time specification is particularly brain-damage one; it looks
+# like ISO 8601, but the T delimiter is missing, the 'S' at the end means
+# who-knows-what and the month field is 0-based. Makes total sense, right?
+
+sub to_moment
+{
+  my $tm = shift;
+
+  $tm =~ /^(\d{4})(\d{2})(\d{2})(\d{6})S$/;
+  $tm = sprintf('%04d%02d%02dT%sZ', $1, $2+1, $3, $4);
+  return Time::Moment->from_string($tm);
+}
+
+
 #=== state initialization/loading ============================================
 
 my $state_file = path($cfg{state});
@@ -49,10 +77,10 @@ my $state = {};
 if(-f $state_file) {
   say 'State file exists, loading';
   $state = $js->decode($state_file->slurp_raw());
-};
+}
 
 
-#=== processing of logfiles ==================================================
+#=== loading of logfiles =====================================================
 
 my $logdir = path($cfg{logdir});
 
@@ -62,17 +90,72 @@ foreach my $server (keys %{$cfg{servers}}) {
 
   foreach my $log (qw(log milestones)) {
   
+    # get URL and localfile
     my $url = $cfg{servers}{$server}{$log}{url};
     my $file = $logdir->child($cfg{servers}{$server}{$log}{file});
-  
-    say $url;
-    say $file;
 
+    # get our last position in the file (or 0 if none)
+    my $fpos = $state->{servers}{$server}{$log}{fpos} // 0;  
+
+    # retrieve new data from URL
     my $r = system(sprintf($cfg{wget}, $file, $url));
     die "Failed to get $url" if $r;
+
+    # open the file and seek into it
+    open(my $fh, '<', $file) or die 'Failed to open ' . $file;
+    seek($fh, $fpos, 0) if $fpos;
+
+    # read the new data
+    my ($count_total, $count_selected) = (0,0);
+    while(my $line = <$fh>) {
+      chomp $line;
+      my %row;
+
+      # following regex splits the line by ':' delimiter, but ignores '::',
+      # which works as an escape to denote ':' in value
+      foreach my $fv (split(/(?<!:):(?!:)/, $line)) {
+        $fv =~ s/::/:/g;
+        my @fv = split(/=/, $fv);
+        $row{$fv[0]} = $fv[1];
+      }
+
+      $count_total++;
+
+      # check for team members, ignore all other entries
+      next if !(grep { $_ eq $row{name} } @{$cfg{match}{members}});
+
+      # convert dates into epoch format
+      if($log eq 'log') {
+        my $tm_start = to_moment($row{start});
+        next if $tm_start < $cfg{match}{start};
+        $row{start_epoch} = $tm_start->epoch;
+        my $tm_end = to_moment($row{end});
+        next if $tm_end >= $cfg{match}{end};
+        $row{end_epoch} = $tm_end->epoch;
+      } else {
+        my $tm_time = to_moment($row{time});
+        next if $tm_time < $cfg{match}{start} || $tm_time >= $cfg{match}{end};
+        $row{time_epoch} = to_moment($row{time})->epoch;
+      }
+
+      $count_selected++;
+    }
+
+    printf(
+      "  %d new lines (%d matched) in %s/%s\n",
+      $count_total, $count_selected, $server, $log
+    );
+
+    # finish
+    $fpos = tell($fh);
+    $state->{servers}{$server}{$log}{fpos} = $fpos;
+    close($fh);
+
   }
 
 }
+
+#=== processing of data ======================================================
 
 
 #=== save state ==============================================================
